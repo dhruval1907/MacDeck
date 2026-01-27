7672d120c8e086889085e2f46ef6d0730b584701
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-27 18:30:05 +0530

    notes-part

--- src/windows/Notes.jsx
+++ src/windows/Notes.jsx
@@ -1,25 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import Markdown from 'react-markdown'
 import MacWindow from './MacWimdow'
-
+import "./notes.scss"
 
 const Notes = () => {
     const [markdown, setmarkdown] = useState(null)
 
     useEffect(() => {
-        fetch("/notes.txt")
+        fetch("/note.txt")
             .then(res => res.text())
             .then(text => setmarkdown(text))
+            .catch(err => console.error(err))
     }, [])
 
-    console.log(markdown);
-    
+
     return (
-        <div>
-            <div className='note-window'>
-                {markdown ? <MacWindow>{markdown}</MacWindow>: <h1>Loading...</h1>}
-            </div>
+       <MacWindow>
+        <div className='note-window'>
+            {markdown ? <Markdown>{markdown}</Markdown> : <p>Loading....</p>}
         </div>
+       </MacWindow>
     )
 }
 
